82e72e652ea84b4f1b23ed43886785b4f61c2e1a
Inna Lapina <jyliana@gmail.com>
2024-10-21 18:52:17 +0300

    adding a couple of files

---
+++ kafka-ms-order/src/main/java/com/course/kafka/api/responce/OrderResponse.java
@@ -0,0 +1,14 @@
+package com.course.kafka.api.responce;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponse {
+
+  private String orderNumber;
+
+}
